e62164586a12032946ee596e174f6f7caaa1c5f9
Diego Nova <diegonova368@gmail.com>
2025-11-19 18:44:25 -0500

    Creando el repositorio y PUNTO de control antes de hacer la logica de crear laptop, receta y detalleReceta

--- MRPBaseDatosII/MRPBaseDatosII/Servicios/RepositorioLaptop.cs
+++ MRPBaseDatosII/MRPBaseDatosII/Servicios/RepositorioLaptop.cs
@@ -2,8 +2,15 @@
 {
     public interface IRepositorioLaptop
     {
+        
     }
     public class RepositorioLaptop
     {
+        private readonly string connectionString;
+        public RepositorioLaptop(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
     }
 }
