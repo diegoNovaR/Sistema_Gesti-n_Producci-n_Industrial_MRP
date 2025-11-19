688d8699280c07616b40e7c8996c31925d6a0a90
Diego Nova <diegonova368@gmail.com>
2025-11-19 12:49:40 -0500

    Agregando archivos modelos

---
+++ MRPBaseDatosII/MRPBaseDatosII/Models/DetalleReceta.cs
@@ -0,0 +1,11 @@
+﻿namespace MRPBaseDatosII.Models
+{
+    public class DetalleReceta
+    {
+        public int Id { get; set; }
+        public int IdReceta { get; set; }
+        public int IdMateriaPrima { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal CostoTotalMateriaPrima { get; set; }
+    }
+}
---
+++ MRPBaseDatosII/MRPBaseDatosII/Models/Receta.cs
@@ -0,0 +1,11 @@
+﻿namespace MRPBaseDatosII.Models
+{
+    public class Receta
+    {
+        public int Id { get; set; }
+        public int IdLaptop { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public decimal CostoTotalDeReceta { get; set; }
+    }
+}
