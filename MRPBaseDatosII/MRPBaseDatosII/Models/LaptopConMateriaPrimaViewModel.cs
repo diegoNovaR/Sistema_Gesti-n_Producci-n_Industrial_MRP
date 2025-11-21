ea24f8b86f0a1ca2b37af06ec6aae05827b908de
Diego Nova <diegonova368@gmail.com>
2025-11-21 12:03:51 -0500

    Configurando vista Laptop crear y arreglando select de la misma. Se agrego mas atributos a laptopMateriaViewModel

--- MRPBaseDatosII/MRPBaseDatosII/Models/LaptopConMateriaPrimaViewModel.cs
+++ MRPBaseDatosII/MRPBaseDatosII/Models/LaptopConMateriaPrimaViewModel.cs
@@ -4,6 +4,13 @@
     {
         public Laptop? Laptop { get; set; }
         public List<MateriaPrimaVM>? MateriasPrimas { get; set; }
+        public int ProcesadorId { get; set; }
+        public int RamId { get; set; }
+        public int AlmacenamientoId { get; set; }
+        public int PlacaMadreId { get; set; }
+        public int PantallaId { get; set; }
+        public int TarjetaVideoId { get; set; }
+        public int ChasisId { get; set; }
     }
     public class MateriaPrimaVM
     {
