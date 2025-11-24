983e2ab5fe2a9a24c7da9e06ed6a99578d5aae95
Diego Nova <diegonova368@gmail.com>
2025-11-24 18:11:53 -0500

    agregando clase y vista de inventario

--- MRPBaseDatosII/MRPBaseDatosII/Models/InventarioLaptopDTO.cs
+++ MRPBaseDatosII/MRPBaseDatosII/Models/InventarioLaptopDTO.cs
@@ -1,6 +1,13 @@
 ﻿namespace MRPBaseDatosII.Models
 {
-    public class InventarioLaptop
+    public class InventarioLaptopDTO
     {
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+        public decimal precioVenta { get; set; }
+        public int stock { get; set; }
+        public int stock_minimo { get; set; }
+        public string ubicacion_seccion { get; set; }
+        public string ubicacion_stand { get; set; }
     }
 }
